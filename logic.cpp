fc1d708348b7914fa8fdc46e509524fb3dc874d8
Nafis Sajid <nafissajidns@gmail.com>
2020-04-20 15:43:43 +0600

    logic

---
+++ logic.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a < 10)
+    {
+        printf("Less than 10\n");
+    }
+    else
+    {
+        printf("Greater than 10\n");
+    }
+}
